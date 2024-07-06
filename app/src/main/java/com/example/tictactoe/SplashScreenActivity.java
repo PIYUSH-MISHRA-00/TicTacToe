f5543b019406edda97d0d5e7e8c62ee37964c7a6
PIYUSH-MISHRA-00 <piyushmishra.professional@gmail.com>
2024-07-06 17:25:28 +0530

    added the lottie animations

--- app/src/main/java/com/example/tictactoe/SplashScreenActivity.java
+++ app/src/main/java/com/example/tictactoe/SplashScreenActivity.java
@@ -3,25 +3,21 @@ package com.example.tictactoe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(intent);
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
                 finish();
             }
-        }, 2000);
+        }, 3000); // 3 seconds delay
     }
 }
